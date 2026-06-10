e1064b9406aef4368984a46d74c7a3738c725a56
Jefri Bulo <jefribulomakassar@gmail.com>
2026-06-10 18:07:06 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
@@ -14,19 +9,26 @@ const nextConfig = {
         tls: false,
         child_process: false,
       };
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "node:util": "util",
+        "node:events": "events",
+        "node:stream": "stream-browserify",
+        "node:buffer": "buffer",
+        "node:crypto": "crypto-browserify",
+        "node:path": "path-browserify",
+        "node:os": "os-browserify",
+        "node:http": "stream-http",
+        "node:https": "https-browserify",
+        "node:zlib": "browserify-zlib",
+      };
     }
     return config;
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "drive.google.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
+      { protocol: "https", hostname: "drive.google.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
