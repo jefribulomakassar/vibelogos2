7bdd1595f264ed8e08837f91b6761245ab5072c9
Jefri Bulo <jefribulomakassar@gmail.com>
2026-06-10 18:02:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
